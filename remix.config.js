0a4fb0cd7f0b1a1f110f98b56645a1a0efd79dbc
Jack G <jackrgisel@gmail.com>
2023-03-11 09:39:45 -0800

    changing the setup for vercel

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: 'vercel',
+  server: ['test', 'development'].includes(process.env.NODE_ENV)
+    ? undefined
+    : './server.js',
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
+  future: {},
 };
